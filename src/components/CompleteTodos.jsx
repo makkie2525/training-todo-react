79276f639a8465ee0eef31e841102d24feddccd4
makkie2525 <18.25.21rymarsa@gmail.com>
2024-03-16 18:56:15 +0900

    component化

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,27 @@
+const style = {
+    border: "2px solid #aacfd0",
+    width: "400px",
+    minHeight: "200px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px",
+    backgroundColor: "#c9dede"
+}
+export const CompleteTodos = (props) =>{
+    const {todos, onClickReturn}=props;
+    return(
+        <div style={style}>
+          <p className="title">完了済のtodo</p>
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={todo}>
+                <div className="list-row">
+                  <p className="todo-item">{todo}</p>
+                  <button onClick={() => onClickReturn(index)}>戻す</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+    );
+};
